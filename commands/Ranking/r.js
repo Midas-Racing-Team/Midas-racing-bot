d851d146cf08d0e3dfec80d19160950cf5541e91
Cyoxite <ander.polanski@gmail.com>
2022-04-08 18:07:52 +0200

    Added switch case to ranking table.

--- commands/Ranking/r.js
+++ commands/Ranking/r.js
@@ -6,10 +6,10 @@ const {
 
 // CONFIG:
 
-let resultlink = 'https://www.republicofsimracers.com/timetable/live-data/906?racenum=q&server=1';
-let resulttitle = 'Race 897 ranking';
-let resulturl = 'https://www.republicofsimracers.com/'; //This URL redirect from title button
-let resulticon = 'https://i.imgur.com/lm8s41J.png';
+let resultlink = 'https://www.republicofsimracers.com/timetable/live-data/898?server=1';
+let resulttitle = 'OPEN Series S4: Road America';
+let resulturl = 'https://www.republicofsimracers.com/event/open-series-s4/road-america?server=1&racenum=q'; //This URL redirect from title button
+let resulticon = 'https://www.republicofsimracers.com/storage/img/tracks/maps/6GCcQIkd4cpmJpblw0YI5MYrpzN5YIlKQhgcbmdN.webp';
 let embedcolor = 3147003;
 
 // END OF CONFIG
@@ -22,26 +22,30 @@ module.exports = {
     execute: async (message, args) => {
         axios.get(resultlink)
             .then((res) => {
+                
 
-                //Make result accessibility for all functions
-                let resd = res;
+                let resd = res; //Make result accessibility for all functions
+                var messagessend = 0;
+                // Calculate positions to show
                 var countpositions = 0;
-                // We need to calculate max finish position for specify messages to send
-                for (key in resd.data) {
+                 for (key in resd.data) {
                     countpositions++
-                    // Here we need to set max finishposition number from res
-                    console.log("klucz" + countpositions);
+                     var highestposition = countpositions;
+                    
                 }
 
-                var higherposition = 22; // temporary variable for max messages function. Edit function above
+                Howmanymessages = Math.ceil(highestposition / 15); //Every message can show 15 positions
 
 
+                console.log("Have " + highestposition + " racers to show.");
+                console.log("And " + Howmanymessages + " messages to send.");
                 
+
                 //Create first message with 'table' headers
 
                 $firstmessage = [{
                         name: "Msc",
-                        value: '__ 1 __',
+                        value: "__" + resd.data[0].finishposition + "__",
                         inline: true
                     },
                     {
@@ -58,18 +62,19 @@ module.exports = {
                 ];
 
 
-                function morestats(n) {
+                function morestats(c,n) {
 
-                    var c = n - 13;
+                    
 
 
                     for (var i = c; i < n; i += 2) {
-                        console.log(i);
+                        
                         pos1 = i;
                         pos2 = i;
                         pos2++;
+                        
                         if (typeof $morepositions === 'undefined') {
-
+                            
                             var $morepositions = {
                                 table: [{
                                         name: resd.data[pos1].finishposition.toString(),
@@ -88,77 +93,138 @@ module.exports = {
                                     },
                                 ]
                             };
-                            //console.log($morepositions);
+                            
+                            
                         } else {
-                            $morepositions.table.push({
-                                name: resd.data[pos1].finishposition.toString(),
-                                value: resd.data[pos2].finishposition.toString(),
-                                inline: true
-                            })
-                            $morepositions.table.push({
-                                name: resd.data[pos1].name,
-                                value: resd.data[pos2].name,
-                                inline: true
-                            })
-                            $morepositions.table.push({
-                                name: resd.data[pos1].laptimebest,
-                                value: resd.data[pos2].laptimebest,
-                                inline: true
-                            })
+                            if(pos2 >= highestposition || pos1 > highestposition){
+                                    pos1position = "---";
+                                    pos1name = "---";
+                                    pos1time = "---";
+                                    pos2position = "---";
+                                    pos2name = "---";
+                                    pos2time = "---";
+                                   
+                            }else{
+                                    pos1position = resd.data[pos1].finishposition.toString();
+                                    pos1name = resd.data[pos1].name;
+                                    pos1time = resd.data[pos1].laptimebest;
+                                    pos2position = resd.data[pos2].finishposition.toString();
+                                    pos2name = resd.data[pos2].name;
+                                    pos2time = resd.data[pos2].laptimebest;
+                            };
 
-
-                            //console.log($morepositions);
-                            //console.log(pos1 + " and " + pos2);
+                            $morepositions.table.push({
+                                name: pos1position,
+                                value: pos2position,
+                                inline: true
+                            })
+                            $morepositions.table.push({
+                                name: pos1name,
+                                value: pos2name,
+                                inline: true
+                            })
+                            $morepositions.table.push({
+                                name: pos1time,
+                                value: pos2time,
+                                inline: true
+                            })
+                            
+                            
+                        
                         }
-
+                            
                     };
-                    //console.log($morepositions);
+                    
                     return $morepositions.table;
                 };
 
 
+                while(messagessend < Howmanymessages){
+                switch (messagessend){
+
+                    case 0:
+                        // TODO: Add IF racers under 5 break before morestats
+                        maxposition = 14;
+                        if(maxposition > highestposition){
+                            maxposition = highestposition;
+                        };
+                        
+                        namepos="Miejsca 1-15";
+                        
+                        sendmessage(namepos,$rankingMessage = [
+                            $firstmessage, morestats(1,maxposition)
+                        ]);
+                        console.log("Send first message.");
+                        messagessend++;
+                        break;
+                    
+                    case 1:
+                        
+                        minposition = 15;
+                        maxposition = 29;
+                        if(maxposition > highestposition){
+                            maxposition = highestposition;
+                            maxposition;
+                        };
+                        namepos="Miejsca 16-"+ maxposition;
+                        
+                        sendmessage(namepos,$rankingMessage = [
+                             morestats(minposition,maxposition)
+                        ]);
+                        console.log("Send second message.");
+                        messagessend++;
+                        break;
+                        
+                    case 2:
+                        minposition = 30;
+                        maxposition = 44;
+                        if(maxposition > highestposition){
+                            maxposition = highestposition;
+                            maxposition--;
+                        };
+                        namepos="Miejsca 31-"+ maxposition;
+                        
+                        sendmessage(namepos,$rankingMessage = [
+                             morestats(minposition,maxposition)
+                        ]);
+                        messagessend++;
+                        console.log("Send third message.");
+                        break;
+                    
+                    case 3:
+                        minposition = 45;
+                        maxposition = 59;
+                        if(maxposition > highestposition){
+                            maxposition = highestposition;
+                            maxposition--;
+                        };
+                        namepos="Miejsca 46-"+ maxposition;
+                        
+                        sendmessage(namepos,$rankingMessage = [
+                             morestats(minposition,maxposition)
+                        ]);
+                        console.log("Send fourth message.");
+                        messagessend++;
+                        break;
+                }
+            };
 
 
-
-
-
-                // Here we send message
-
-                message.channel.send({
-                    embeds: [{
-                        color: embedcolor,
-                        author: {
-                            name: "Miejsca 1-15",
-                            icon_url: resulticon
-                        },
-                        title: resulttitle,
-                        url: resulturl,
-                        fields: $rankingMessage = [
-                            $firstmessage, morestats(14)
-                        ],
-
-
-
-                    }]
-                });
-                //next message
-                message.channel.send({
-                    embeds: [{
-                        color: embedcolor,
-                        author: {
-                            name: "Miejsca 15-30",
-                            icon_url: resulticon
-                        },
-                        title: resulttitle,
-                        url: resulturl,
-                        fields: $rankingMessage = [
-                            morestats(25)
-                        ],
-
-
-
-                    }]
-                });
+            
+            function sendmessage(namepos,$rankingMessage){
+            message.channel.send({
+                embeds: [{
+                    color: embedcolor,
+                    author: {
+                        name: namepos,
+                        icon_url: resulticon
+                    },
+                    title: resulttitle,
+                    url: resulturl,
+                    fields: $rankingMessage,
+                }]
+            });
+        }
 
 
             })
--- commands/Ranking/ranking.js
+++ commands/Ranking/ranking.js
@@ -1,112 +1,233 @@
 const Discord = require(`discord.js`);
 const axios = require(`axios`);
-const { MessageEmbed } = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 
+// CONFIG:
 
-    
+let resultlink = 'https://www.republicofsimracers.com/timetable/live-data/898?server=1';
+let resulttitle = 'OPEN Series S4: Road America';
+let resulturl = 'https://www.republicofsimracers.com/event/open-series-s4/road-america?server=1&racenum=q'; //This URL redirect from title button
+let resulticon = 'https://www.republicofsimracers.com/storage/img/tracks/maps/6GCcQIkd4cpmJpblw0YI5MYrpzN5YIlKQhgcbmdN.webp';
+let embedcolor = 3147003;
+
+// END OF CONFIG
+
 module.exports = {
     name: `ranking`,
     aliases: ['ranking'],
     description: `ranks`,
     usage: `!ranking`,
-    execute: async(message, args) => {
-        axios.get(`https://www.republicofsimracers.com/timetable/preq/905?race=897`)
-        .then((res) => {
-            //console.log(`RES:`, res.data.data[0]);
-           let resd = res;
-        //We downloaded data, now create first message
+    execute: async (message, args) => {
+        axios.get(resultlink)
+            .then((res) => {
                 
-        function morestats(n){
 
-            for ( var i=1;i<25;i+=2){
-                   pos1 = i;
-                   pos2 = i + 1;
-    if (typeof $morepositions === 'undefined') {
-                  
-       var $morepositions = { table: [{
-                name: pos1.toString(),
-                value:  pos2.toString(),
-                inline: true
-            },
-            {
-              name: resd.data.data[pos1].name,
-              value: resd.data.data[pos2].name,
-              inline: true
-            },
-            {
-              name: resd.data.data[pos1].laptime,
-              value: resd.data.data[pos2].laptime ,
-              inline: true
-            },]};
-            console.log($morepositions);
-            console.log(i);
-            } else {
-                $morepositions.table.push({
-                    name: pos1.toString(),
-                    value:  pos2.toString(),
-                    inline: true
-                })
-                $morepositions.table.push({
-                    name: resd.data.data[pos1].name,
-                    value: resd.data.data[pos2].name,
-                    inline: true
-                })
-                $morepositions.table.push({
-                    name: resd.data.data[pos1].laptime,
-                    value: resd.data.data[pos2].laptime ,
-                    inline: true
-                })
+                let resd = res; //Make result accessibility for all functions
+                var messagessend = 0;
+                // Calculate positions to show
+                var countpositions = 0;
+                 for (key in resd.data) {
+                    countpositions++
+                     var highestposition = countpositions;
+                    
+                }
+
+                Howmanymessages = Math.ceil(highestposition / 15); //Every message can show 15 positions
+
+
+                console.log("Have " + highestposition + " racers to show.");
+                console.log("And " + Howmanymessages + " messages to send.");
                 
-                
-                    console.log($morepositions);
-            }
 
-    };
-    return $morepositions.table;
-        };
-        
+                //Create first message with 'table' headers
 
-        $rankingMessage =[
-        {
-            name: "Msc",
-            value:  '__ 1 __',
-            inline: true
-          },
-          {
-            name: "Imię i Nazwisko",
-            value: "__" + res.data.data[0].name + "__",
-            inline: true
-          },
-          {
-            name: "Czas",
-            value: "__" + res.data.data[0].laptime + "__",
-            inline: true
-          }, morestats(5) 
-        
-        
-        ];
-        
-           
+                $firstmessage = [{
+                        name: "Msc",
+                        value: "__" + resd.data[0].finishposition + "__",
+                        inline: true
+                    },
+                    {
+                        name: "Imię i Nazwisko",
+                        value: "__" + resd.data[0].name + "__",
+                        inline: true
+                    },
+                    {
+                        name: "Czas",
+                        value: "__" + resd.data[0].laptimebest + "__",
+                        inline: true
+                    }
+
+                ];
 
 
+                function morestats(c,n) {
 
-          message.channel.send({ embeds: [{
-    color: 3447003,
-    author: {
-      name: "Author Name, it can hold 256 characters",
-      icon_url: "https://i.imgur.com/lm8s41J.png"
-    },
-    title: "Race 897 ranking",
-    url: "https://www.republicofsimracers.com/",
-    fields: $rankingMessage,
-    
-   
-    
-  }]});
-        })
-        .catch((err) => {
-            console.error(`ERR:`, err)
-        })
+                    
+
+
+                    for (var i = c; i < n; i += 2) {
+                        
+                        pos1 = i;
+                        pos2 = i;
+                        pos2++;
+                        
+                        if (typeof $morepositions === 'undefined') {
+                            
+                            var $morepositions = {
+                                table: [{
+                                        name: resd.data[pos1].finishposition.toString(),
+                                        value: resd.data[pos2].finishposition.toString(),
+                                        inline: true
+                                    },
+                                    {
+                                        name: resd.data[pos1].name,
+                                        value: resd.data[pos2].name,
+                                        inline: true
+                                    },
+                                    {
+                                        name: resd.data[pos1].laptimebest,
+                                        value: resd.data[pos2].laptimebest,
+                                        inline: true
+                                    },
+                                ]
+                            };
+                            
+                            
+                        } else {
+                            if(pos2 >= highestposition || pos1 > highestposition){
+                                    pos1position = "---";
+                                    pos1name = "---";
+                                    pos1time = "---";
+                                    pos2position = "---";
+                                    pos2name = "---";
+                                    pos2time = "---";
+                                   
+                            }else{
+                                    pos1position = resd.data[pos1].finishposition.toString();
+                                    pos1name = resd.data[pos1].name;
+                                    pos1time = resd.data[pos1].laptimebest;
+                                    pos2position = resd.data[pos2].finishposition.toString();
+                                    pos2name = resd.data[pos2].name;
+                                    pos2time = resd.data[pos2].laptimebest;
+                            };
+
+                            $morepositions.table.push({
+                                name: pos1position,
+                                value: pos2position,
+                                inline: true
+                            })
+                            $morepositions.table.push({
+                                name: pos1name,
+                                value: pos2name,
+                                inline: true
+                            })
+                            $morepositions.table.push({
+                                name: pos1time,
+                                value: pos2time,
+                                inline: true
+                            })
+                            
+                            
+                        
+                        }
+                            
+                    };
+                    
+                    return $morepositions.table;
+                };
+
+
+                while(messagessend < Howmanymessages){
+                switch (messagessend){
+
+                    case 0:
+                        // TODO: Add IF racers under 5 break before morestats
+                        maxposition = 14;
+                        if(maxposition > highestposition){
+                            maxposition = highestposition;
+                        };
+                        
+                        namepos="Miejsca 1-15";
+                        
+                        sendmessage(namepos,$rankingMessage = [
+                            $firstmessage, morestats(1,maxposition)
+                        ]);
+                        console.log("Send first message.");
+                        messagessend++;
+                        break;
+                    
+                    case 1:
+                        
+                        minposition = 15;
+                        maxposition = 29;
+                        if(maxposition > highestposition){
+                            maxposition = highestposition;
+                            maxposition;
+                        };
+                        namepos="Miejsca 16-"+ maxposition;
+                        
+                        sendmessage(namepos,$rankingMessage = [
+                             morestats(minposition,maxposition)
+                        ]);
+                        console.log("Send second message.");
+                        messagessend++;
+                        break;
+                        
+                    case 2:
+                        minposition = 30;
+                        maxposition = 44;
+                        if(maxposition > highestposition){
+                            maxposition = highestposition;
+                            maxposition--;
+                        };
+                        namepos="Miejsca 31-"+ maxposition;
+                        
+                        sendmessage(namepos,$rankingMessage = [
+                             morestats(minposition,maxposition)
+                        ]);
+                        messagessend++;
+                        console.log("Send third message.");
+                        break;
+                    
+                    case 3:
+                        minposition = 45;
+                        maxposition = 59;
+                        if(maxposition > highestposition){
+                            maxposition = highestposition;
+                            maxposition--;
+                        };
+                        namepos="Miejsca 46-"+ maxposition;
+                        
+                        sendmessage(namepos,$rankingMessage = [
+                             morestats(minposition,maxposition)
+                        ]);
+                        console.log("Send fourth message.");
+                        messagessend++;
+                        break;
+                }
+            };
+
+
+            
+            function sendmessage(namepos,$rankingMessage){
+            message.channel.send({
+                embeds: [{
+                    color: embedcolor,
+                    author: {
+                        name: namepos,
+                        icon_url: resulticon
+                    },
+                    title: resulttitle,
+                    url: resulturl,
+                    fields: $rankingMessage,
+                }]
+            });
+        }
+
+
+            })
+            .catch((err) => {
+                console.error(`ERR:`, err)
+            })
     }
 }
-
